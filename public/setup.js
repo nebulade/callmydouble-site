78a17618542e551bddd0c3117d33e2287f452327
Johannes Zellner <webmaster@nebulon.de>
2013-12-16 10:41:42 -0800

    Make app keys regenerateable

--- public/setup.js
+++ public/setup.js
@@ -6,9 +6,24 @@ var BACKEND_URL = 'http://localhost:3001/api/v1/';
 
 function getApplicationDetails(callback) {
     var form = document.getElementById('form-generate');
-    var auth = { email: form.elements.namedItem('email').value, password: form.elements.namedItem('password').value };
+    var email = form.elements.namedItem('email').value;
+    var password = form.elements.namedItem('password').value;
 
-    superagent.post(BACKEND_URL + 'details').send(auth).end(function (error, result) {
+    superagent.post(BACKEND_URL + 'apps/0/details').withCredentials().auth(email, password).end(function (error, result) {
+        if (error || !result.ok) {
+            return callback(error ? error : result.status);
+        }
+
+        callback(null, result.body);
+    });
+}
+
+function refreshApplicationDetails(callback) {
+    var form = document.getElementById('form-generate');
+    var email = form.elements.namedItem('email').value;
+    var password = form.elements.namedItem('password').value;
+
+    superagent.post(BACKEND_URL + 'apps/0/generate').withCredentials().auth(email, password).end(function (error, result) {
         if (error || !result.ok) {
             return callback(error ? error : result.status);
         }
@@ -18,7 +33,7 @@ function getApplicationDetails(callback) {
 }
 
 function signin(email, password) {
-    superagent.post(BACKEND_URL + 'login').send({ email: email, password: password }).end(function (error, result) {
+    superagent.post(BACKEND_URL + 'users/signin').withCredentials().auth(email, password).end(function (error, result) {
         if (error || !result.ok) {
             console.error('Failed to sign in.', error, result && result.status);
             return;
@@ -34,8 +49,8 @@ function signin(email, password) {
                 return;
             }
 
-            document.getElementById('application-key').innerHTML = result.key;
-            document.getElementById('application-secret').innerHTML = result.secret;
+            document.getElementById('application-key').innerHTML = result.appKey;
+            document.getElementById('application-secret').innerHTML = result.appSecret;
             document.getElementById('box-signin').style.display = 'none';
             document.getElementById('box-application').style.display = 'block';
         });
@@ -48,6 +63,18 @@ function init() {
     form.addEventListener('submit', function (event) {
         signin(form.elements.namedItem('email').value, form.elements.namedItem('password').value);
         event.preventDefault();
+    });
+
+    document.getElementById('app-refresh-button').addEventListener('click', function (event) {
+        refreshApplicationDetails(function (error, result) {
+            if (error) {
+                console.error('Failed to get details.', error);
+                return;
+            }
+
+            document.getElementById('application-key').innerHTML = result.appKey;
+            document.getElementById('application-secret').innerHTML = result.appSecret;
+        });
     });
 }
 
