caa754b2f191ac05124cbf0915375cf03653ed8d
Johannes Zellner <webmaster@nebulon.de>
2013-12-30 08:20:05 -0800

    Add jshint and a watcher to the gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,22 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: ['src/*.js'],
+            options: {
+                curly: true,
+                eqeqeq: true,
+                eqnull: true,
+                regexdash: true,
+                browser: true,
+                trailing: true,
+                forin: true,
+                strict: true,
+                jquery: true,
+                node: true,
+                indent: 4
+            },
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -53,16 +69,35 @@ module.exports = function(grunt) {
         },
         clean: [
             'dist/'
-        ]
+        ],
+        watch: {
+            js: {
+                files: ['src/*.js'],
+                tasks: ['jshint', 'uglify']
+            },
+            hbs: {
+                files: ['src/*.hbs'],
+                tasks: ['assemble']
+            },
+            html: {
+                files: ['src/*.html'],
+                tasks: ['copy']
+            },
+            css: {
+                files: ['src/*.css'],
+                tasks: ['copy']
+            }
+        }
     });
 
     // Load the plugins
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('assemble');
 
     // Default task(s).
-    grunt.registerTask('default', ['clean', 'uglify', 'assemble', 'copy']);
-
+    grunt.registerTask('default', ['clean', 'jshint', 'uglify', 'assemble', 'copy']);
 };
